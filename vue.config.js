ea9494bd8eb62aa6dfd01ccb790f545d36fb895c
rocsmytune <smz0705@163.com>
2023-10-29 21:42:11 +0800

    #add: get new gencode with questionSubmit

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  // 开启使用MonacoEditor
   chainWebpack(config) {
     config.plugin("monaco").use(new MonacoWebpackPlugin());
   },
